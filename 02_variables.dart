c95b0ad4fc03cb7dd860dc2cef1cccfa2d243ddf
gabriel-079 <contatogc871@gmail.com>
2023-02-21 14:44:21 -0300

    adidding files

---
+++ 02_variables.dart
@@ -0,0 +1,17 @@
+void main() {
+  //String/Carcactere
+  String fullName = "Gabriel Coutinho";
+  print(fullName);
+
+  //Int
+  int age = 26;
+  print(age);
+
+  //Bolean
+  bool isOfAge = true;
+  print(isOfAge);
+
+  //List
+  List<String> listOfNames = ["Gabriel", "Apollo", "Negan"];
+  print(listOfNames[1]);
+}
